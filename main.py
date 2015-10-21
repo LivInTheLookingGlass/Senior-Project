9beaa2052b4e47a2fd075f4d9b7988b03a38a8ad
Gabe Appleton <gappleto97@gmail.com>
2015-10-20 17:06:34 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import optparse, settings
+
+def main():
+    parser = optparse.OptionParser()
+    
+    parser.add_option('-c',
+                      '--charity',
+                      dest='charity',
+                      default=None,
+                      action="store_true",
+                      help='Sets whether you accept rewardless bounties')
+    parser.add_option('-l',
+                      '--latency',
+                      dest='accept_latency',
+                      default=None,
+                      help='Maximum acceptable latency from a server')
+    parser.add_option('-f',
+                      '--propagation-factor',
+                      dest='propagate-factor',
+                      default=None,
+                      help='Minimum funds:reward ratio you'll propagate bounties at')
