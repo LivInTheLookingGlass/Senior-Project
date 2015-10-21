3de5ba7f9abad1e0cf1a15acf13a74b70eb16f87
Gabe Appleton <gappleto97@gmail.com>
2015-10-20 23:26:11 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,15 @@
 import os, pickle
 
+defaults = ['charity':False,
+            'propagate_factor':2,
+            'accept_latency':2000]
+
 def setup(overrides):
   config = []
   if os.path.exists("settings.conf"):
     config = pickle.load("settings.conf")
   else:
-    config = ['charity':False,
-              'propagate_factor':2,
-              'accept_latency':2000]
+    config = defaults
     pickle.dump(config,"settings.conf")
   for key in overrides:
     if overrides[key] is None:
