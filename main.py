c0a479ad3bbfd0f2f77c628ee10fd01675a942b9
Gabe Appleton <gappleto97@gmail.com>
2015-10-23 13:48:47 -0700

    Move configs to settings module

--- main.py
+++ main.py
@@ -1,37 +1,11 @@
-import optparse
 from bounty import *
 from peers import *
-from settings import *
+import settings
 
 def main():
-    parser = optparse.OptionParser()
-    
-    parser.add_option('-c',
-                      '--charity',
-                      dest='charity',
-                      default=None,
-                      action="store_true",
-                      help='Sets whether you accept rewardless bounties')
-    parser.add_option('-l',
-                      '--latency',
-                      dest='accept_latency',
-                      default=None,
-                      help='Maximum acceptable latency from a server')
-    parser.add_option('-f',
-                      '--propagation-factor',
-                      dest='propagate-factor',
-                      default=None,
-                      help='Minimum funds:reward ratio you'll propagate bounties at')
-                      
-    (options, args) = parser.parse_args()
-    
-    overrides = {}
-    
-    for key in options:
-        if settings.defaults.get(key) is not None:
-            overrides = overrides.update({key:settings.default.get(key)})
-            
-    settings.setup(overrides)
+    settings.setup()
+    print "settings are:"
+    print settings.config
     
 if __name__ == "__main__":
     main()
