d64eb93f6f620a989da6fce5cdd70ff76fd18d8d
Gabe Appleton <gappleto97@gmail.com>
2015-10-23 13:48:14 -0700

    Move configs to settings module

--- settings.py
+++ settings.py
@@ -1,17 +1,42 @@
-import os, pickle
+import optparse, os, pickle
 
-defaults = {'charity':False,
-            'propagate_factor':2,
-            'accept_latency':2000}
+config = {'charity':False,
+          'propagate_factor':2,
+          'accept_latency':2000}
 
-def setup(overrides):
-  config = {}
+def setup():
+  parser = optparse.OptionParser()
+  parser.add_option('-c',
+                    '--charity',
+                    dest='charity',
+                    default=None,
+                    action="store_true",
+                    help='Sets whether you accept rewardless bounties')
+  parser.add_option('-l',
+                    '--latency',
+                    dest='accept_latency',
+                    default=None,
+                    help='Maximum acceptable latency from a server')
+  parser.add_option('-f',
+                    '--propagation-factor',
+                    dest='propagate_factor',
+                    default=None,
+                    help='Minimum funds:reward ratio you\'ll propagate bounties at')        
+  (options, args) = parser.parse_args()
+
+  print "options parsed"
+  overrides = options.__dict__
+  
   if os.path.exists("settings.conf"):
-    config = pickle.load("settings.conf")
+    config.update(pickle.load(open("settings.conf","r")))
+    print overrides
+    print config
   else:
-    config = defaults
-    pickle.dump(config,"settings.conf")
+    pickle.dump(config,open("settings.conf","w"))
+  kill = []
   for key in overrides:
     if overrides.get(key) is None:
-      overrides.pop(key)
+      kill += [key]
+  for key in kill:
+    overrides.pop(key)
   config.update(overrides)
