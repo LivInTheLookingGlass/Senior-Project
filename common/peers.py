b65eabff2e85f8021f7eac66114d1f45a1551760
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-05 14:18:55 -0800

    Fix listening over hostname bug

--- common/peers.py
+++ common/peers.py
@@ -1,6 +1,6 @@
 import os, pickle, select, socket, time
 
-seedlist = [socket.gethostname() + ":44565"]
+seedlist = ["127.0.0.1:44565", "localhost:44565", "10.132.80.128:44565", "10.132.80.251:44565"]
 peerlist = []
 server = socket.socket()
 
@@ -16,6 +16,7 @@ def getFromSeeds():
   for seed in seedlist:
     print seed
     peerlist.extend(requestPeerlist(seed))
+    time.sleep(3)
 
 def requestPeerlist(address):
   con = socket.socket()
@@ -45,7 +46,7 @@ def sendPeerlist(address):
   #send list
 
 def initializePeerConnections():
-  server.bind((socket.gethostname(),44564))
+  server.bind(("localhost",44564))
   getFromFile()
   print "peers fetched from file"
   getFromSeeds()
@@ -59,10 +60,10 @@ def initializePeerConnections():
 def listen():
   print "currently unsupported"
   server = socket.socket()
-  server.bind((socket.gethostname(),44565))
+  server.bind((get_lan_ip(),44565))
   server.listen(5)
   while True:
-    print "listening on", (socket.gethostname(),44565)
+    print "listening on", (get_lan_ip(),44565)
     a, addr = server.accept()
     print "connection accepted"
     b = a.recv(19)
@@ -73,3 +74,34 @@ def listen():
     a.send("Close Signal")
     time.sleep(1)
     a.close()
+
+if os.name != "nt":
+    import fcntl
+    import struct
+
+    def get_interface_ip(ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+                                ifname[:15]))[20:24])
+
+def get_lan_ip():
+    ip = socket.gethostbyname(socket.gethostname())
+    if ip.startswith("127.") and os.name != "nt":
+        interfaces = [
+            "eth0",
+            "eth1",
+            "eth2",
+            "wlan0",
+            "wlan1",
+            "wifi0",
+            "ath0",
+            "ath1",
+            "ppp0",
+            ]
+        for ifname in interfaces:
+            try:
+                ip = get_interface_ip(ifname)
+                break
+            except IOError:
+                pass
+    return ip
