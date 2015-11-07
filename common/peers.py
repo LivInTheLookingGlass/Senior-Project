9520034c092194dd9c266b239ccbd8bad8ea0fc4
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-06 22:13:02 -0800

    Thread-safe version-safe print, Linux debug

--- common/peers.py
+++ common/peers.py
@@ -1,117 +1,5 @@
 import multiprocessing, os, pickle, select, socket, time
-
-seedlist = ["127.0.0.1:44565", "localhost:44565", "10.132.80.128:44565"]
-peerlist = ["10.132.80.138:44565"]
-remove   = []
-
-#constants
-close_signal = "Close Signal"
-peer_request = "Requesting Peers..."
-peers_file   = "data" + os.sep + "peerlist.pickle"
-
-Alive = True
-
-def getFromFile():
-  if os.path.exists(peers_file):
-    peerlist = pickle.load(open(peers_file,"r"))
-
-def saveToFile():
-  if not os.path.exists(peers_file.split(os.sep)[0]):
-    os.mkdir(peers_file.split(os.sep)[0])
-  pickle.dump(peerlist,open(peers_file,"w"))
-
-def getFromSeeds():
-  for seed in seedlist:
-    print seed
-    peerlist.extend(requestPeerlist(seed))
-    time.sleep(1)
-
-def requestPeerlist(address):
-  con = socket.socket()
-  print address
-  try:
-    print (address.split(":")[0],int(address.split(":")[1]))
-    con.connect((address.split(":")[0],int(address.split(":")[1])))
-    con.send(peer_request)
-    connected = True
-    s = ""
-    while connected:
-      a = con.recv(64)
-      print a
-      if not a == close_signal:
-        s += a
-      else:
-        con.close()
-        connected = False
-    return pickle.loads(s)
-  except Exception as e:
-    print "Failed:", type(e)
-    print e
-    remove.extend([address])
-    return []
-
-def sendPeerlist(address):
-  print "currently unsupported"
-  s = pickle.dumps(peerlist)
-  #send list
-
-def initializePeerConnections():
-  getFromFile()
-  print "peers fetched from file"
-  getFromSeeds()
-  print "peers fetched from seedlist"
-  trimPeers()
-  if len(peerlist) < 12:
-    print len(peerlist)
-    newlist = []
-    for peer in peerlist:
-      newlist.extend(requestPeerlist(peer))
-    peerlist.extend(newlist)
-  trimPeers()
-  print "peer network extended"
-  saveToFile()
-  print "peer network saved to file"
-
-def trimPeers():
-  temp = list(set(peerlist))
-  for peer in remove:
-    try:
-      del temp[temp.index(peer)]
-    except:
-      continue
-  del remove[:]
-  del peerlist[:]
-  peerlist.extend(temp)
-
-def listen(port, ID):
-  server = socket.socket()
-  server.bind(("0.0.0.0",port))
-  server.listen(10)
-  prid = "[" + str(ID) + "]"
-  while True:
-    print prid, "listening on", (get_lan_ip(),port)
-    try:
-      a, addr = server.accept()
-      print prid, "connection accepted"
-      b = a.recv(len(peer_request))
-      print prid, "Received:", b
-      if b == peer_request:
-        a.send(pickle.dumps(peerlist + [get_lan_ip()+":"+str(port)]))
-      a.send(close_signal)
-      time.sleep(1)
-      a.close()
-    except Exception as e:
-      print prid, "Failed:", type(e)
-      print e
-
-class listener(multiprocessing.Process):
-  def __init__(self, threadID, port):
-    multiprocessing.Process.__init__(self)
-    self.threadID = threadID
-    self.port = port
-  def run(self):
-    print "hi"
-    listen(self.port,self.threadID)
+from safeprint import safeprint
 
 if os.name != "nt":
     import fcntl
@@ -133,3 +21,119 @@ def get_lan_ip():
             except IOError:
                 pass
     return ip
+
+seedlist = ["127.0.0.1:44565", "localhost:44565", "10.132.80.128:44565"]
+peerlist = [get_lan_ip() + ":44565"]
+remove   = []
+
+#constants
+close_signal = "Close Signal"
+peer_request = "Requesting Peers..."
+peers_file   = "data" + os.sep + "peerlist.pickle"
+
+Alive = True
+
+def getFromFile():
+  if os.path.exists(peers_file):
+    peerlist = pickle.load(open(peers_file,"r"))
+
+def saveToFile():
+  if not os.path.exists(peers_file.split(os.sep)[0]):
+    os.mkdir(peers_file.split(os.sep)[0])
+  pickle.dump(peerlist,open(peers_file,"w"))
+
+def getFromSeeds():
+  for seed in seedlist:
+    safeprint(seed)
+    peerlist.extend(requestPeerlist(seed))
+    time.sleep(1)
+
+def requestPeerlist(address):
+  con = socket.socket()
+  con.settimeout(5)
+  safeprint(address)
+  try:
+    safeprint(address.split(":")[0] + ":" + address.split(":")[1])
+    con.connect((address.split(":")[0],int(address.split(":")[1])))
+    con.send(peer_request)
+    connected = True
+    s = ""
+    while connected:
+      a = con.recv(64)
+      safeprint(a)
+      if not a == close_signal:
+        s += a
+      else:
+        con.close()
+        connected = False
+    return pickle.loads(s)
+  except Exception as e:
+    safeprint("Failed:" + str(type(e)))
+    safeprint(e)
+    remove.extend([address])
+    return []
+
+def sendPeerlist(address):
+  safeprint("currently unsupported")
+  s = pickle.dumps(peerlist)
+  #send list
+
+def initializePeerConnections():
+  getFromFile()
+  safeprint("peers fetched from file")
+  getFromSeeds()
+  safeprint("peers fetched from seedlist")
+  trimPeers()
+  if len(peerlist) < 12:
+    safeprint(len(peerlist))
+    newlist = []
+    for peer in peerlist:
+      newlist.extend(requestPeerlist(peer))
+    peerlist.extend(newlist)
+  trimPeers()
+  safeprint("peer network extended")
+  saveToFile()
+  safeprint("peer network saved to file")
+
+def trimPeers():
+  temp = list(set(peerlist))
+  for peer in remove:
+    try:
+      del temp[temp.index(peer)]
+    except:
+      continue
+  del remove[:]
+  del peerlist[:]
+  peerlist.extend(temp)
+
+def listen(port, ID):
+  server = socket.socket()
+  server.bind(("0.0.0.0",port))
+  server.listen(10)
+  while True:
+    safeprint("listening on " + str(get_lan_ip()) + ":" + str(port))
+    try:
+      a, addr = server.accept()
+      safeprint("connection accepted")
+      b = a.recv(len(peer_request))
+      safeprint("Received: " + str(b))
+      if b == peer_request:
+        a.send(pickle.dumps(peerlist + [get_lan_ip()+":"+str(port)]))
+        time.sleep(0.1)
+      a.send(close_signal)
+      time.sleep(1)
+      a.close()
+      safeprint("connection closed")
+    except Exception as e:
+      safeprint("Failed: " + str(type(e)))
+      safeprint(e)
+
+class listener(multiprocessing.Process):
+  def __init__(self, threadID, port):
+    multiprocessing.Process.__init__(self)
+    self.threadID = threadID
+    self.port = port
+  def run(self):
+    safeprint("listener started")
+    listen(self.port,self.threadID)
+    
