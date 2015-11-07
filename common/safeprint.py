4d1874228f64375a52efaaa553c0fa5933aa38fb
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-06 22:15:27 -0800

    Remove print delay

--- common/safeprint.py
+++ common/safeprint.py
@@ -1,8 +1,6 @@
 import multiprocessing, sys
-from time import sleep
 
 print_lock = multiprocessing.Lock()
 def safeprint(content):
   with print_lock:
     sys.stdout.write("[" + str(multiprocessing.current_process().pid) + "] " +  str(content) + '\r\n')
-    sleep(0.1)
