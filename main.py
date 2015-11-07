1c07565fe34dac943ae86a92a8d9048478aaeea9
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-06 16:00:52 -0800

    Concurrent listening

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from common.bounty import *
 from common.peers import *
 from common import settings
+from time import sleep
 
 def main():
     settings.setup()
     print "settings are:"
     print settings.config
-    if settings.config.get('server') is not True:
-        initializePeerConnections()
-    else:
-        listen()
+    ear = listener(1,44565)
+    ear.daemon = True
+    ear.start()
+    initializePeerConnections()
     
 if __name__ == "__main__":
     main()
