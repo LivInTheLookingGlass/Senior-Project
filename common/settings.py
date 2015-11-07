3c332bdcfc93355c431963853fe39e48d2c18da3
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-06 16:23:14 -0800

    Fix bug if config file not present, but folder is

--- common/settings.py
+++ common/settings.py
@@ -3,6 +3,7 @@ import optparse, os, pickle
 config = {'charity':False,
           'propagate_factor':2,
           'accept_latency':2000}
+settings_file = "data" + os.sep + "settings.conf"
 
 def setup():
   parser = optparse.OptionParser()
@@ -27,25 +28,20 @@ def setup():
                     dest='server',
                     default=None,
                     action="store_true",
-                    help='Sets whether you operate as a server or client (Default: client)')     
-  parser.add_option('-p,
-                    '--port',
-                    dest='port',
-                    default=None,
-                    help=('Port on which to listen (default: ' + str(config['port']) + ')'))
+                    help='Sets whether you operate as a server or client (Default: client)')        
   (options, args) = parser.parse_args()
 
   print "options parsed"
   overrides = options.__dict__
   
-  if os.path.exists("data" + os.sep + "settings.conf"):
-    config.update(pickle.load(open("data" + os.sep + "settings.conf","r")))
+  if os.path.exists(settings_file):
+    config.update(pickle.load(open(settings_file,"r")))
     print overrides
     print config
   else:
-    if not os.path.exists("data" + os.sep + "settings.conf"):
-      os.mkdir("data")
-    pickle.dump(config,open("data" + os.sep + "settings.conf","w"))
+    if not os.path.exists(settings_file.split(os.sep)[0]):
+      os.mkdir(settings_file.split(os.sep)[0])
+    pickle.dump(config,open(settings_file,"w"))
   kill = []
   for key in overrides:
     if overrides.get(key) is None:
