52888fe8a7bcd461ebf315a82686632bb25830dc
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-07 18:27:23 -0800

    Update bounty.py

--- common/bounty.py
+++ common/bounty.py
@@ -1,159 +1,109 @@
-import multiprocessing, os, pickle, select, socket, time
+import os, pickle, re
 from common.safeprint import safeprint
-from common.Bounty import *
 
-if os.name != "nt":
-    import fcntl
-    import struct
+bountyList = []
 
-    def get_interface_ip(ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
-                                ifname[:15]))[20:24])
+class Bounty:
+  ip = ""
+  btc = ""
+  reward = 0
+  data = []
+  
+  def __init__(self, ipAddress, btcAddress, rewardAmount, dataList=[]):
+    self.ip = ipAddress
+    self.btc = btcAddress
+    self.reward = rewardAmount
+    self.data = dataList
+    
+  def isValid(self):
+    try:
+      safeprint("Testing IP address")
+      #is IP valid
+      b = int(self.ip.split(":")[1])
+      b = int(self.ip.split(":")[0].split(".")[0])
+      b = int(self.ip.split(":")[0].split(".")[1])
+      b = int(self.ip.split(":")[0].split(".")[2])
+      b = int(self.ip.split(":")[0].split(".")[3])
+      #ping IP
+      #is Bitcoin address valid
+      safeprint("Testing Bitcoin address")
+      address = str(self.btc)
+      #The following is a soft check
+      #A deeper check will need to be done in order to assure this is correct
+      if not re.match(re.compile("^[a-zA-Z1-9]{27,35}$"),address):
+	return False
+      #is reward valid
+      safeprint("Testing reward")
+      b = int(self.reward)
+      return (b >= 0)
+    except:
+      return False
+  
+  def isPayable(self):
+    #check if address has enough
+    return False
 
-def get_lan_ip():
-    ip = socket.gethostbyname(socket.gethostname())
-    if ip.startswith("127.") and os.name != "nt":
-        interfaces = ["eth0","eth1","eth2","wlan0","wlan1","wifi0","ath0","ath1","ppp0",]
-        for ifname in interfaces:
-            try:
-                ip = get_interface_ip(ifname)
-                break
-            except IOError:
-                pass
-    return ip
-
-seedlist = ["127.0.0.1:44565", "localhost:44565", "10.132.80.128:44565"]
-peerlist = [get_lan_ip() + ":44565"]
-remove   = []
-bounties = []
-
-#constants
-peers_file      = "data" + os.sep + "peerlist.pickle"
-close_signal    = "Close Signal......."
-peer_request    = "Requesting Peers..."
-incoming_bounty = "Incoming Bounty...."
-valid_signal    = "Bounty was valid..."
-invalid_signal  = "Bounty was invalid."
-
-Alive = True
-
-def getFromFile():
-  if os.path.exists(peers_file):
-    peerlist = pickle.load(open(peers_file,"r"))
+def verify(string):
+  test = pickle.loads(string)
+  try:
+    safeprint("Testing IP address")
+    #is IP valid
+    b = int(self.ip.split(":")[1])
+    b = int(self.ip.split(":")[0].split(".")[0])
+    b = int(self.ip.split(":")[0].split(".")[1])
+    b = int(self.ip.split(":")[0].split(".")[2])
+    b = int(self.ip.split(":")[0].split(".")[3])
+    #ping IP
+    #is Bitcoin address valid
+    safeprint("Testing Bitcoin address")
+    address = str(self.btc)
+    #The following is a soft check
+    #A deeper check will need to be done in order to assure this is correct
+    if not re.match(re.compile("^[a-zA-Z1-9]{27,35}$"),address):
+      return False
+    #is reward valid
+    safeprint("Testing reward")
+    b = int(self.reward)
+    return (b >= 0)
+  except:
+    return False
 
 def saveToFile():
-  if not os.path.exists(peers_file.split(os.sep)[0]):
-    os.mkdir(peers_file.split(os.sep)[0])
-  pickle.dump(peerlist,open(peers_file,"w"))
+  if os.path.exists("bounties.pickle"):
+    pickle.dump(boutyList,"bounties.pickle")
+    return True
+  return False
 
-def getFromSeeds():
-  for seed in seedlist:
-    safeprint(seed)
-    peerlist.extend(requestPeerlist(seed))
-    time.sleep(1)
+def loadFromFile():
+  if os.path.exists("settings.conf"):
+    bountyList = pickle.load("bounties.pickle")
+    return True
+  return False
 
-def requestPeerlist(address):
-  con = socket.socket()
-  con.settimeout(5)
-  safeprint(address)
-  try:
-    safeprint(address.split(":")[0] + ":" + address.split(":")[1])
-    con.connect((address.split(":")[0],int(address.split(":")[1])))
-    con.send(peer_request)
-    connected = True
-    s = ""
-    while connected:
-      a = con.recv(64)
-      safeprint(a)
-      if not a == close_signal:
-        s += a
-      else:
-        con.close()
-        connected = False
-    return pickle.loads(s)
-  except Exception as e:
-    safeprint("Failed:" + str(type(e)))
-    safeprint(e)
-    remove.extend([address])
-    return []
+def loadBounties():
+  loadFromFile()
+  if len(bountyList) is 0:
+    requestBounties()
+  return len(bountyList) is not 0
 
-def sendPeerlist(address):
+def requestBounties(peerList):
+  for peer in peerList:
+    bountyList.extend(requestBounty(peer))
+    
+def requestBounty(peer):
   safeprint("currently unsupported")
-  s = pickle.dumps(peerlist)
-  #send list
 
-def initializePeerConnections():
-  getFromFile()
-  safeprint("peers fetched from file")
-  getFromSeeds()
-  safeprint("peers fetched from seedlist")
-  trimPeers()
-  if len(peerlist) < 12:
-    safeprint(len(peerlist))
-    newlist = []
-    for peer in peerlist:
-      newlist.extend(requestPeerlist(peer))
-    peerlist.extend(newlist)
-  trimPeers()
-  safeprint("peer network extended")
-  saveToFile()
-  safeprint("peer network saved to file")
+def sendBounty(peer):
+  safeprint("currently unsupported")
+  if len(bountyList) is 0:
+    loadBounties()
+  #send bounties
+  dumpBounties()
 
-def trimPeers():
-  temp = list(set(peerlist))
-  for peer in remove:
-    try:
-      del temp[temp.index(peer)]
-    except:
-      continue
-  del remove[:]
-  del peerlist[:]
-  peerlist.extend(temp)
-
-def listen(port, ID):
-  server = socket.socket()
-  server.bind(("0.0.0.0",port))
-  server.listen(10)
-  while True:
-    safeprint("listening on " + str(get_lan_ip()) + ":" + str(port))
-    try:
-      a, addr = server.accept()
-      safeprint("connection accepted")
-      b = a.recv(len(peer_request))
-      safeprint("Received: " + str(b))
-      if b == peer_request:
-        a.send(pickle.dumps(peerlist + [get_lan_ip()+":"+str(port)]))
-        time.sleep(0.1)
-      elif b == incoming_bounty:
-	connected = True
-	s = ""
-	while connected:
-	  c = a.recv(len(close_signal))
-	  safeprint(c)
-	  if not c == close_signal:
-	    s += c
-	  else:
-	    con.close()
-	    connected = False
-	if (verify(s)):
-	  Bounty.bountyList.append(bount)
-	  a.send(valid_signal)
-	else:
-	  a.send(invalid_signal)
-      a.send(close_signal)
-      time.sleep(1)
-      a.close()
-      safeprint("connection closed")
-    except Exception as e:
-      safeprint("Failed: " + str(type(e)))
-      safeprint(e)
-
-class listener(multiprocessing.Process):
-  def __init__(self, threadID, port):
-    multiprocessing.Process.__init__(self)
-    self.threadID = threadID
-    self.port = port
-  def run(self):
-    safeprint("listener started")
-    listen(self.port,self.threadID)
+def getBounty(charity, factor):
+  for bounty in bountyList:
+    if best is None:
+      best = bounty
+    elif best.rewardAmount < bounty.rewardAmount and bounty.isValid() and (isPayable(factor) or charity):
+      best = bounty
+  return best
