75db80fcb7ee95866e2c97644d76ae37faffb8d0
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-09 20:33:03 -0800

    Update marked.js

--- marked.js
+++ marked.js
@@ -776,10 +776,10 @@ Renderer.prototype.code = function(code, lang, escaped) {
       + '\n</code></pre>';
   }
 
-  return '<pre><code class="'
+  return '<pre class="'
     + this.options.langPrefix
     + escape(lang, true)
-    + '">'
+    + '"><code>'
     + (escaped ? code : escape(code, true))
     + '\n</code></pre>\n';
 };
