38b7aa811f79c6550a6a8d7eb34410f596a51b18
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-10 17:02:57 -0800

    Update footnoter.js

--- javascripts/footnoter.js
+++ javascripts/footnoter.js
@@ -2,6 +2,9 @@ footnum = 1
 
 function addFootnote(message)  {
   document.write("<a href=\"footnote" + footnum + "\"><sup>" + footnum + "</sup></a>");
+  console.log("<a href=\"footnote" + footnum + "\"><sup>" + footnum + "</sup></a>");
   $("#footnotes").append("<a name=\"footnote" + footnum + "\">" + footnum + "</a>: <p>" + message + "</p>");
+  console.log("<a name=\"footnote" + footnum + "\">" + footnum + "</a>: <p>" + message + "</p>");
   footnum = footnum + 1;
+  console.log(footnum);
 }
