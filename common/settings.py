2d49a5eeffc6354800b1778d20cac3854e37bbea
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-12 13:46:05 -0800

    Add options

--- common/settings.py
+++ common/settings.py
@@ -3,8 +3,8 @@ from common.safeprint import safeprint
 
 config = {'accept_latency':2000,
           'charity':False,
-          'port':44565,
           'outbound':False,
+          'port':44565,
           'propagate_factor':2,
           'server':False}
 settings_file = "data" + os.sep + "settings.conf"
@@ -26,16 +26,30 @@ def setup():
                       default=None,
                       action="store_true",
                       help='Sets whether you accept rewardless bounties')
-    parser.add_option('-l',
-                      '--latency',
-                      dest='accept_latency',
-                      default=None,
-                      help='Maximum acceptable latency from a server')
     parser.add_option('-f',
                       '--propagation-factor',
                       dest='propagate_factor',
                       default=None,
+                      type="int",
                       help='Minimum funds:reward ratio you\'ll propagate bounties at')
+    parser.add_option('-l',
+                      '--latency',
+                      dest='accept_latency',
+                      default=None,
+                      type="int",
+                      help='Maximum acceptable latency from a server')
+    parser.add_option('-o',
+                      '--outbound-only',
+                      dest='outbound',
+                      default=None,
+                      action="store_false",
+                      help='Maximum acceptable latency from a server')
+    parser.add_option('-p',
+                      '--listening-port',
+                      dest='port',
+                      default=None,
+                      type="int",
+                      help='Port for the program to listen at')
     parser.add_option('-S',
                       '--server',
                       dest='server',
