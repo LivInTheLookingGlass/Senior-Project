794a75ed410fe39ba2376ebcab75d21cc5e9fee0
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-20 23:23:41 -0800

    Reduce the amount of time locking

--- common/safeprint.py
+++ common/safeprint.py
@@ -1,6 +1,7 @@
 import multiprocessing, sys, datetime
 
-print_lock = multiprocessing.Lock()
+print_lock = multiprocessing.RLock()
 def safeprint(content):
+  string = "[" + str(multiprocessing.current_process().pid) + "] " + datetime.datetime.now().strftime('%H%M%S') + ": " + str(content) + '\r\n'
   with print_lock:
-    sys.stdout.write(("[" + str(multiprocessing.current_process().pid) + "] " + datetime.datetime.now().strftime('%H%M%S') + ": " + str(content) + '\r\n'))
+    sys.stdout.write(string)
