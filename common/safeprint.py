322c83165a6be09f214c400f58d0c23b5acef157
Gabe Appleton <gappleto97+development@gmail.com>
2015-11-23 13:12:48 -0800

    Try this silliness to get windows output [skip ci]

--- common/safeprint.py
+++ common/safeprint.py
@@ -5,4 +5,6 @@ def safeprint(content):
     """Prints in a thread-lock, taking a single object as an argument"""
     string = "[" + str(multiprocessing.current_process().pid) + "] " + datetime.datetime.now().strftime('%H:%M:%S') + ": " + str(content) + '\r\n'
     with print_lock:
+        with open("output.txt","ab") as log:
+            log.write(string)
         sys.stdout.write(string)
