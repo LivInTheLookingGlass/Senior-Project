a3d03c938b1d04f7abc8fed367ad2f583eef5780
Gabe Appleton <gappleto97+development@gmail.com>
2015-12-10 21:26:21 -0800

    Update installpypy3.ps1

--- installpypy3.ps1
+++ installpypy3.ps1
@@ -1,5 +1,4 @@
-(New-Object Net.WebClient).DownloadFile('https://bitbucket.org/pypy/pypy/downloads/pypy3-2.4.0-win32.zip', '$env:appveyor_build_folder\\pypy3-2.4.0-win32.zip')
-(New-Object Net.WebClient).DownloadFile('https://bootstrap.pypa.io/get-pip.py', '$env:appveyor_build_folder\\get-pip.py')
-7z x pypy-4.0.1-win32.zip
-$env:path = '$env:appveyor_build_folder\\pypy3-2.4.0-win32;$env:path'
-pypy get-pip.py
+\Python27\python -c "import urllib; urllib.urlretrieve ('http://bitbucket.org/pypy/pypy/downloads/pypy3-2.4.0-win32.zip', r'pypy3-2.4.0-win32.zip')"
+\Python27\python -c "import urllib; urllib.urlretrieve ('https://bootstrap.pypa.io/get-pip.py', r'get-pip.py')"
+7z x pypy3-2.4.0-win32.zip > Out-Null
+pypy3-2.4.0-win32\\pypy get-pip.py
