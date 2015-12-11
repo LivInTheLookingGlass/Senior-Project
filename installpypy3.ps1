ceeb7ab6992337a246dce2e3cf6f2661cb12cae0
Gabe Appleton <gappleto97+development@gmail.com>
2015-12-10 22:16:40 -0800

    add setuptools [skip ci]

--- installpypy3.ps1
+++ installpypy3.ps1
@@ -2,3 +2,4 @@
 \Python27\python -c "import urllib; urllib.urlretrieve ('https://bootstrap.pypa.io/get-pip.py', r'get-pip.py')"
 7z x pypy3-2.4.0-win32.zip > Out-Null
 pypy3-2.4.0-win32\\pypy get-pip.py
+pypy3-2.4.0-win32\\pypy -m pip install setuptools
