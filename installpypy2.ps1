aa2fd649cb7dc0b7909f2f5d2c4c6244deb86b11
Gabe Appleton <gappleto97+development@gmail.com>
2015-12-10 19:36:28 -0800

    Update installpypy2.ps1

--- installpypy2.ps1
+++ installpypy2.ps1
@@ -1,5 +1,4 @@
 \Python27\python -c "import urllib; urllib.urlretrieve ('http://bitbucket.org/pypy/pypy/downloads/pypy-4.0.1-win32.zip', r'pypy-4.0.1-win32.zip')"
 \Python27\python -c "import urllib; urllib.urlretrieve ('https://bootstrap.pypa.io/get-pip.py', r'get-pip.py')"
 7z x pypy-4.0.1-win32.zip
-$env:path = '$env:appveyor_build_folder\\pypy-4.0.1-win32;$env:path'
-pypy get-pip.py
+pypy pypy-4.0.1-win32\\get-pip.py
