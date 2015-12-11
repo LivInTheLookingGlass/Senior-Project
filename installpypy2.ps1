396ec09ef62d7da5591f46ca80cb4fd6aa894528
Gabe Appleton <gappleto97+development@gmail.com>
2015-12-10 19:10:41 -0800

    Update installpypy2.ps1

--- installpypy2.ps1
+++ installpypy2.ps1
@@ -1,5 +1,5 @@
-(New-Object Net.WebClient).DownloadFile('https://bitbucket.org/pypy/pypy/downloads/pypy-4.0.1-win32.zip', '$env:appveyor_build_folder\\pypy-4.0.1-win32.zip')
-(New-Object Net.WebClient).DownloadFile('https://bootstrap.pypa.io/get-pip.py', '$env:appveyor_build_folder\\get-pip.py')
+(New-Object Net.WebClient).DownloadFile('http://bitbucket.org/pypy/pypy/downloads/pypy-4.0.1-win32.zip', '$env:appveyor_build_folder\\pypy-4.0.1-win32.zip')
+(New-Object Net.WebClient).DownloadFile('http://bootstrap.pypa.io/get-pip.py', '$env:appveyor_build_folder\\get-pip.py')
 7z x pypy-4.0.1-win32.zip
 $env:path = '$env:appveyor_build_folder\\pypy-4.0.1-win32;$env:path'
 pypy get-pip.py
