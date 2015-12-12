c3504c59cbd289ee99fc6c0f43626760cc95d8ac
Gabe Appleton <gappleto97+development@gmail.com>
2015-12-11 20:33:40 -0800

    [skip ci]

---
+++ upload_coverage.py
@@ -0,0 +1,6 @@
+import platform
+
+if platform.python_implementation() != "PyPy" or sys.version_info[0] != 3:
+  import subprocess
+  subprocess.call("\\Python" + str(sys.version_info[0]) + str(sys.version_info[1]) + "\\Scripts\\coverage xml")
+  subprocess.call("\\Python" + str(sys.version_info[0]) + str(sys.version_info[1]) + "\\Scripts\\codecov --token=0482d032-e24c-461b-a116-f0e3dbc88734")
