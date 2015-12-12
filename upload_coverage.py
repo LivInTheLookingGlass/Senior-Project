2002080db5a85cfcbb5bae99dbc58aa4fbfa50bd
Gabe Appleton <gappleto97+development@gmail.com>
2015-12-11 21:20:19 -0800

    Update upload_coverage.py

--- upload_coverage.py
+++ upload_coverage.py
@@ -4,6 +4,7 @@ if platform.python_implementation() != "PyPy" or sys.version_info[0] != 3:
   import subprocess
   subprocess.call("\\Python" + str(sys.version_info[0]) + str(sys.version_info[1]) + "\\Scripts\\coverage xml")
   a = "\\Python" + str(sys.version_info[0]) + str(sys.version_info[1])
+  print(platform.architecture()[0])
   if platform.architecture()[0] != '32bit':
     a += "-X64"
   subprocess.call(a + "\\Scripts\\codecov --token=0482d032-e24c-461b-a116-f0e3dbc88734")
