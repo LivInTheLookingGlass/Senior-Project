4b9a1f01160d36b7dff67cf07e65a64a877af3fb
Gabe Appleton <gappleto97+development@gmail.com>
2015-12-11 19:16:22 -0800

    Update test.py

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ def waitForty(v,q): #pragma: no cover
         sleep(4)
         requestBounties("localhost:44566")
         sleep(4)
-        safeprint(bounty.bountyList)
+        safeprint(bounty.getBountyList())
     #TODO move test incoming_bounty here
     #TODO move test incoming_bounty here
     safeprint("Sending term signal")
