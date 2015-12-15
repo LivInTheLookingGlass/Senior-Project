00509af9c27b3e63ebcf377879631f3553b7d241
Gabe Appleton <gappleto97+development@gmail.com>
2015-12-14 19:01:59 -0800

    [skip ci]

--- installpypy3.ps1
+++ installpypy3.ps1
@@ -1,5 +1,4 @@
 \Python27\python -c "import urllib; urllib.urlretrieve ('http://bitbucket.org/pypy/pypy/downloads/pypy3-2.4.0-win32.zip', r'pypy3-2.4.0-win32.zip')"
-\Python27\python -c "import urllib; urllib.urlretrieve ('https://bootstrap.pypa.io/get-pip.py', r'get-pip.py')"
 7z x pypy3-2.4.0-win32.zip > Out-Null
 pypy3-2.4.0-win32\\pypy get-pip.py
 pypy3-2.4.0-win32\\pypy -m pip install setuptools
