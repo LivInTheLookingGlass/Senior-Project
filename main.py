2e428a1df29915ca9e63baf35af5d994607b8c23
Gabe Appleton <gappleto97+development@gmail.com>
2016-01-12 20:54:40 -0800

    Address some codeclimate issues

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from common import bounty
-from common.peers import *
-from common import settings
+from common import bounty, peers, settings
+from common.safeprint import safeprint
 from multiprocessing import Queue, Value
 from time import sleep, time
 import pickle
 
 def sync():
     from multiprocessing import Manager
-    from common import bounty, settings, peers
-    from common.safeprint import safeprint
     man = Manager()
     items = {'config':man.dict(),
              'peerList':man.list(),
@@ -19,26 +16,12 @@ def sync():
     items['peerList'].extend(peers.peerlist)
     items['bountyList'].extend(bounty.bountyList)
     safeprint(items)
-    safeprint(items.get('bountyList'))
-    safeprint(items.get('keyList'))
-    if items.get('config') is not None:
-        from common import settings
-        settings.config = items.get('config')
-    if items.get('peerList') is not None:
-        global peerList
-        peers.peerlist = items.get('peerList')
-    if items.get('bountyList') is not None:
-        from common import bounty
-        bounty.bountyList = items.get('bountyList')
-    if items.get('bountyLock') is not None:
-        from common import bounty
-        bounty.bountyLock = items.get('bountyLock')
+    peers.sync(items)
     return items
 
 def main():
     #Begin Init
     settings.setup()
-    from common.safeprint import safeprint
     try:
         import miniupnpc
     except:
@@ -48,11 +31,11 @@ def main():
     safeprint(settings.config)
     queue = Queue()
     live = Value('b',True)
-    ear = listener(settings.config['port'],settings.config['outbound'],queue,live,settings.config['server'])
+    ear = peers.listener(settings.config['port'],settings.config['outbound'],queue,live,settings.config['server'])
     ear.daemon = True
     ear.items = sync()
     ear.start()
-    mouth = propagator(settings.config['port'] + 1, live)
+    mouth = peers.propagator(settings.config['port'] + 1, live)
     mouth.daemon = True
     mouth.items = ear.items
     mouth.start()
@@ -65,13 +48,13 @@ def main():
         feedback = queue.get(False)
     except: #pragma: no cover
         safeprint("No feedback received from listener")
+    global ext_ip, ext_port
     ext_ip = ""     #Does this affect peers?
     ext_port = -1   #Does this affect peers?
     if feedback != []:
         settings.outbound = feedback[0]
         if settings.outbound is not True:
-            ext_ip = feedback[1]
-            ext_port = feedback[2]
+            ext_ip, ext_port = feedback[1:3]
     initializePeerConnections(settings.config['port'], ext_ip, ext_port)
     #End Init
     
@@ -93,7 +76,7 @@ def main():
     safeprint("Beginning exit process")
     live.value = False
     settings.saveSettings()
-    saveToFile()
+    peers.saveToFile()
     bounty.saveToFile()
     #End shutdown
 
