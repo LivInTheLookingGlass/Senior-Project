b543005ee87daf04b42a817e35ebf689f4a136b9
Gabe Appleton <gappleto97+development@gmail.com>
2016-01-12 23:41:51 -0800

    More style guide issues [skip ci]

--- cert_auth.py
+++ cert_auth.py
@@ -1,15 +1,16 @@
-import pickle, rsa, os
+import pickle, os
+from rsa import PrivateKey, sign, verify
 
 if __name__ == "__main__":
     master_tup = pickle.load(open("masterKey.pickle", "rb"))
-    master_key = rsa.PrivateKey(*master_tup)
+    master_key = PrivateKey(*master_tup)
 
     if os.path.exists("to_sign"):
         for key in os.listdir("." + os.sep + "to_sign"):
             tup = pickle.load(open("to_sign" + os.sep + key, "rb"))
-            signed = rsa.sign(str(tup), master_key, "SHA-256")
-            print("Key " + str(key) + " signed")
-            print("Signiture valid? " + str(rsa.verify(str(tup), signed, master_key)))
+            signed = sign(str(tup), master_key, "SHA-256")
+            print("Key " + str(key) + " signed.")
+            print("Sig valid? " + str(verify(str(tup), signed, master_key)))
             if not os.path.exists("signed"):
                 os.mkdir("signed")
             pickle.dump(signed, open("signed" + os.sep + key, "wb"), 0)
@@ -17,8 +18,8 @@ if __name__ == "__main__":
 
 def exportProof(priv):
     tup = (priv.n, priv.e)
-    proof = rsa.sign(str(tup), priv,"SHA-256")
+    proof = rsa.sign(str(tup), priv, "SHA-256")
     print("Post this proof somewhere associated with your")
     print("organization, to verify your identity.")
     print("Send that link to gappleto97+development@gmail.com")
-    return (tup,proof)
+    return (tup, proof)
