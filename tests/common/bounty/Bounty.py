4c8c4d8f959896aef8ea1d98feb1d95c7f804c60
Gabe Appleton <gappleto97+development@gmail.com>
2016-02-06 19:10:48 -0800

    Fix more style guide issues

--- tests/common/bounty/Bounty.py
+++ tests/common/bounty/Bounty.py
@@ -12,42 +12,42 @@ def validation():
     num_invalid = 10
     # Begin adding to check-list
     l += [(Bounty(('8.8.8.8', -1), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 1440),
-              "Bounty with invalid ip address (invalid port)")]
+          "Bounty with invalid ip address (invalid port)")]
     l += [(Bounty(('8.8.8', 8888), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 1440),
-              "Bounty with invalid ip address (incorrect number of bytes)")]
+          "Bounty with invalid ip address (incorrect number of bytes)")]
     l += [(Bounty(('8.8.8.8', 49153), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 1440),
-              "Bounty with invalid ip address (invalid port)")]
+          "Bounty with invalid ip address (invalid port)")]
     l += [(Bounty(('8.8.256.8', 8), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 1440),
-              "Bounty with invalid ip address (overflowed byte in address)")]
+          "Bounty with invalid ip address (overflowed byte in address)")]
     l += [(Bounty(('8.8.8.8', 8), "1JTGcHS3GMhBGGww4ZEDmP7u9", 1440),
-              "Bounty with invalid bitcoin address")]
+          "Bounty with invalid bitcoin address")]
     l += [(Bounty(('8.8.8.8', 8), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 1439),
-              "Bounty with invalid reward amount (outside of valid range)")]
+          "Bounty with invalid reward amount (outside of valid range)")]
     l += [(Bounty(('8.8.8.8', 8), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 0),
-              "Bounty with invalid reward amount (reward of zero with no sig)")]
+          "Bounty with invalid reward amount (reward of zero with no sig)")]
     l += [(Bounty(('8.8.8.8', 8), "1LhPsd4ng3AXmVPzrLWprWFx351pW4HJm8", 1440, timeout=1),
-              "Bounty that has timed out")]
+          "Bounty that has timed out")]
     l += [(Bounty(('8.8.8.8', 8), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 1440,
-                     data={'reqs': {("sys", "platform", "index=2",
-                                     "end=3"): "win33"}}),
-              "Bounty with invalid requirements (would not work on any system")]
+                 data={'reqs': {("sys", "platform", "index=2",
+                                "end=3"): "win33"}}),
+          "Bounty with invalid requirements (would not work on any system")]
     l += [(Bounty(('8.8.8.8', 8), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 1440,
-                     data={'reqs': {("__builtin__", "pow", 2, 2): 4, ("sys", "platform"): "w"}}),
-              "Bounty with invalid requirements (including some valid ones)")]
+                 data={'reqs': {("__builtin__", "pow", 2, 2): 4,
+                               ("sys", "platform"): "w"}}),
+          "Bounty with invalid requirements (including some valid ones)")]
     l += [(Bounty(('8.8.8.8', 8), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 1440),
-              "Correctly formed bounty")]
+          "Correctly formed bounty")]
     l += [(Bounty(('8.8.8.8', 8), "1LhPsd4ng3AXmVPzrLWprWFx351pW4HJm8", 10900),
-              "Correctly formed bounty")]
+          "Correctly formed bounty")]
     l += [(Bounty(('8.8.8.8', 8), "1MWSdYMKEpfWVxC6BGYARxsksaQuyEWzG5", 1480),
-              "Correctly formed bounty")]
+          "Correctly formed bounty")]
     l += [(Bounty(('8.8.8.8', 8), "1EgGfDetymjMzcQ1AEhHjHEyXHjnEavwgg", 10290),
-              "Correctly formed bounty")]
+          "Correctly formed bounty")]
     l += [(Bounty(('8.8.8.8', 8), "1JTGcHS3GMhBGLcFRuHLk6Gww4ZEDmP7u9", 1440,
-                     data={'reqs': {("__builtin__", "pow", 2, 2): 4}}),
-              "Correctly formed bounty with system requirements")]
+                 data={'reqs': {("__builtin__", "pow", 2, 2): 4}}),
+          "Correctly formed bounty with system requirements")]
     # Begin testing list
     for i in range(len(l)):
-        print i
         if bool(i >= num_completely_invalid) ^ bool(l[i][0].isValid()):
             return (False, l[i][1])
     return (True, "All bounties correctly assessed")
