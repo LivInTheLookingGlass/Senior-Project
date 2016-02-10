4ace9edb7432b5c0de677924301477ce86480486
Gabe Appleton <gappleto97+development@gmail.com>
2016-02-10 09:25:19 -0800

    Make thread marker consistent length

--- common/safeprint.py
+++ common/safeprint.py
@@ -3,12 +3,15 @@ from datetime import datetime
 from common import settings
 
 print_lock = multiprocessing.RLock()
-
+max_digits = 0
 
 def safeprint(msg, verbosity=0):
     """Prints in a thread-lock, taking a single object as an argument"""
-    string = ("[" + str(multiprocessing.current_process().pid) + "] " +
-              datetime.now().strftime('%H:%M:%S: ') + str(msg) + '\r\n')
+    pid = str(multiprocessing.current_process().pid)
+    max_digits = max(max_digits, len(pid))
+    pid = pid.zfill(max_digits)
+    string = ("[" + pid + "] " + datetime.now().strftime('%H:%M:%S: ') +
+              str(msg) + '\n')
     with print_lock:
         with open("output.txt", "a") as log:
             log.write(string)
