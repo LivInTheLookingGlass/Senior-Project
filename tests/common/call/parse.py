e9b6cdaeba69aaa5f6cfefea61d701fcbb8ff884
Gabe Appleton <gappleto97+development@gmail.com>
2016-02-11 19:40:19 -0800

    Fix style guide issues [skip ci]

--- tests/common/call/parse.py
+++ tests/common/call/parse.py
@@ -23,7 +23,7 @@ def accuracy():
     l += [("__builtin__", "max", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
     r += [10]
     l += [("math", "pow", 5, 8)]
-    r += [math.pow(5,8)]
+    r += [math.pow(5, 8)]
     l += [("sys", "platform")]
     r += [sys.platform]
     l += [("sys", "maxint")]
